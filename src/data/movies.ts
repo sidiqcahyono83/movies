5113806817c30d3a6214ac56b63e9e756dbcfac2
cahyono muslim sidiq <cdq.cahyono@gmail.com>
2024-06-21 11:42:53 +0700

    modified .yaml

--- src/data/movies.ts
+++ src/data/movies.ts
@@ -5,7 +5,7 @@ type Movie = {
   director: string;
   actors: string[];
   producedBy: string;
-  releaseDate: string;
+  releaseDate: Date;
   genre: string[];
 };
 
@@ -17,7 +17,7 @@ export const dataMovies: Movie[] = [
     director: "Christopher Nolan",
     actors: ["Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page, Tom Hardy"],
     producedBy: "Warner Bros.",
-    releaseDate: "2010-07-16",
+    releaseDate: new Date("2010-07-16"),
     genre: ["Sci-Fi, Action, Adventure"],
   },
   {
@@ -27,7 +27,7 @@ export const dataMovies: Movie[] = [
     director: "Christopher Nolan",
     actors: ["Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine"],
     producedBy: "Warner Bros.",
-    releaseDate: "2008-07-18",
+    releaseDate: new Date("2008-07-18"),
     genre: ["Action, Crime, Drama"],
   },
   {
@@ -37,7 +37,7 @@ export const dataMovies: Movie[] = [
     director: "Luis Llosa",
     actors: ["Tom Berenger, Billy Zane, J.T. Walsh, Aden Young"],
     producedBy: "TriStar Pictures",
-    releaseDate: "1993-01-29",
+    releaseDate: new Date("1993-01-29"),
     genre: ["Action, Thriller, War"],
   },
   {
@@ -49,7 +49,7 @@ export const dataMovies: Movie[] = [
       "Mark Wahlberg, Helena Bonham Carter, Tim Roth, Michael Clarke Duncan",
     ],
     producedBy: "20th Century Fox",
-    releaseDate: "2001-07-27",
+    releaseDate: new Date("2001-07-27"),
     genre: ["Sci-Fi, Action, Adventure"],
   },
 ];
--- src/index.ts
+++ src/index.ts
@@ -5,120 +5,108 @@ const app = new Hono();
 let movies = dataMovies;
 
 app.get("/", (c) => {
-	return c.json({
-		message: "Movie API",
-		URL: "/movies",
-	});
+  return c.json({
+    message: "Movie API",
+    URL: "/movies",
+  });
 });
 
 app.get("/movies", (c) => {
+  if (movies.length <= 0) {
+    return c.json({ massage: "Data movies is none, please seeds data movies" });
+  }
 
-	if(movies.length <= 0){
-		return c.json({massage:"Data movies is none, please seeds data movies"});
-	}
-
-	return c.json(movies);
+  return c.json(movies);
 });
 
 app.get("/movies/:id", (c) => {
-	const id = Number(c.req.param("id"));
-	const movie = dataMovies.find((movies) => movies.id === id);
-	if (!movie) {
-		return c.json({ message: "movie not found" });
-	}
-	return c.json(movie);
+  const id = Number(c.req.param("id"));
+  const movie = dataMovies.find((movies) => movies.id === id);
+  if (!movie) {
+    return c.json({ message: "movie not found" });
+  }
+  return c.json(movie);
 });
 
 app.delete("/movies/:id", (c) => {
-	
-	const id = Number(c.req.param("id"));
-	const movie = movies.find((movies) => movies.id === id);
+  const id = Number(c.req.param("id"));
+  const movie = movies.find((movies) => movies.id === id);
 
-	if (!movie) {
-		return c.json({ message: "Movies Not Found" });
-	}
+  if (!movie) {
+    return c.json({ message: "Movies Not Found" });
+  }
 
-	movies = movies.filter((movies) => movies.id !== id);
+  movies = movies.filter((movies) => movies.id !== id);
 
-	return c.json(`movies by Title ${id} deleted`);
+  return c.json(`movies by Title ${id} deleted`);
 });
 
 app.delete("/movies", (c) => {
-	movies = []
+  movies = [];
 
-	return c.json({massage: "All movies succes deleted"});
+  return c.json({ massage: "All movies succes deleted" });
 });
 
-app.post("/movies/seeds", (c)=>{
-	movies=dataMovies
+app.post("/movies/seeds", (c) => {
+  movies = dataMovies;
 
-	return c.json({massage: "Data movies have succes seedes"})
-})
+  return c.json({ massage: "Data movies have succes seedes" });
+});
 
 app.post("/movies", async (c) => {
+  const { title, duration, director, actors, producedBy, releaseDate, genre } =
+    await c.req.json();
 
-	const { title, duration,director,
-		actors,
-		producedBy,
-		releaseDate,
-		genre, } = await c.req.json();
+  const nextId = movies[movies.length - 1].id + 1;
 
-	const nextId = movies[movies.length - 1].id + 1;
+  const newMovie = {
+    id: nextId,
+    title,
+    duration,
+    director,
+    actors,
+    producedBy,
+    releaseDate,
+    genre,
+  };
 
-	const newMovie = {
-		id: nextId,
-		title,
-		duration,
-		director,
-		actors,
-		producedBy,
-		releaseDate,
-		genre,
-	};
+  movies = [...movies, newMovie];
 
-	movies = [...movies, newMovie];
-
-	return c.json(newMovie);
+  return c.json(newMovie);
 });
-
 
 app.put("movies/:id", async (c) => {
+  const id = Number(c.req.param("id"));
+  const movie = movies.find((movie) => movie.id === id);
 
-	const id = Number(c.req.param("id"));
-	const movie = movies.find((movie) => movie.id === id);
+  if (!movie) {
+    return c.json({ massage: `Movie by ${id} not found` });
+  }
+  const { title, duration, director, actors, producedBy, releaseDate, genre } =
+    await c.req.json();
 
-	if (!movie) {
-		return c.json({ massage: `Movie by ${id} not found` });
-	}
-	const { title, duration,director,
-		actors,
-		producedBy,
-		releaseDate,
-		genre, } = await c.req.json();
+  const newMovie = {
+    id,
+    title,
+    duration,
+    director,
+    actors,
+    producedBy,
+    releaseDate,
+    genre,
+  };
 
-	const newMovie = {
-		id,
-		title,
-		duration,
-		director,
-		actors,
-		producedBy,
-		releaseDate,
-		genre,
-	};
-
-	movies = movies.map((movie)=>{
-		if(movie.id === id){
-			return newMovie
-		}else{
-			return movie
-		}
-	})
-	return c.json(newMovie);
+  movies = movies.map((movie) => {
+    if (movie.id === id) {
+      return newMovie;
+    } else {
+      return movie;
+    }
+  });
+  return c.json(newMovie);
 });
 
-const port = 3000
+const port = 3000;
 console.log(`Rest Movies run in PORT: ${port}`);
-
 
 export default app;
